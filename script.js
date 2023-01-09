354bdc1954358a9e7a5f474200b8b0a7302af702
Rajpal-26 <120132958+Rajpal-26@users.noreply.github.com>
2023-01-09 10:43:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+let h3 = document.getElementById("letterCount");
+h3 = prompt("enter text");
+let count = 0;
+for (i = 0; i < h3.length; i++){
+    if (h3[i] == " ") {
+        count=0;
+    }
+    else {
+        count++;
+    }
+}
+console.log(count);
